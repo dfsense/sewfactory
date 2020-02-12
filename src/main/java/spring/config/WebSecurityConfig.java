fcdd6c7a16a360860e4fc66bc77f6ba0c8ba0730
Дмитрий <df.kovalev@gmail.com>
2020-02-12 06:32:12 +0200

    добавил отключение spring секьюрити

--- src/main/java/spring/config/WebSecurityConfig.java
+++ src/main/java/spring/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
         http
                 .authorizeRequests()
                 .antMatchers("/static/**").permitAll()
--- src/main/resources/static/js/custom/categories.js
+++ src/main/resources/static/js/custom/categories.js
@@ -2,15 +2,22 @@ function createCategory(){
     var data = {}
     data["name"]= $('#inputName').val();
     data["memo"]= $('#inputDescr').val();
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    console.log("header: " + header + ", token: " + token);
     $.ajax({
         url: "/api/category/add",
         type: "POST",
-        contentType: "application/json",
+        headers: {
+            'header': header,
+            'token':token,
+            'Content-Type':'application/json'
+        },
         dataType: "json",
         data: JSON.stringify(data),
         timeout : 100000,
         success: function(jsonData){
-            $('#content_p').html(jsonData)
+            $('#content_p').text(JSON.stringify(jsonData));
         }
     })
 }
